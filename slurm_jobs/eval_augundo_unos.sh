e0937b800656d8deef86c8c252fe6ede90c4a80e
Owen Xu Li <ox4@login1.bouchet.ycrc.yale.edu>
2026-03-24 15:54:51 -0400

    sh file updates

--- slurm_jobs/eval_augundo_unos.sh
+++ slurm_jobs/eval_augundo_unos.sh
@@ -16,7 +16,7 @@ source augundo-ext/augundo-py310env/bin/activate
 
 SENIOR_THESIS="${SLURM_SUBMIT_DIR:-$(cd "$(dirname "$0")/.." && pwd)}"
 
-CHECKPOINT_DIR="$SENIOR_THESIS/augundo-ext/checkpoints/augundo_unos_new"
+CHECKPOINT_DIR="$SENIOR_THESIS/augundo-ext/checkpoints/augundo_unos"
 
 # Use CHECKPOINT_FILE env var, or default to the final checkpoint
 if [[ -z "${CHECKPOINT_FILE}" ]]; then
